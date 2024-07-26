aa1219c861ec9f51944f7dc8f3f49a8fc2d7f02b
TarikSoğukpinar <tariksogukpinar@outlook.com>
2024-07-26 14:48:05 +0300

    chore: env variables installed

--- api/main.go
+++ api/main.go
@@ -2,16 +2,28 @@ package main
 
 import (
 	"log"
+	"os"
 
 	"github.com/gofiber/fiber/v2"
+	"github.com/joho/godotenv"
 )
 
 func main() {
 	app := fiber.New()
 
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "3000"
+	}
+
 	app.Get("/", func(c *fiber.Ctx) error {
 		return c.SendString("Merhaba, Fiber!")
 	})
 
-	log.Fatal(app.Listen(":3000"))
+	log.Fatal(app.Listen(":" + port))
 }
